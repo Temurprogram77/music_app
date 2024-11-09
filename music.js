532623fb2e51de4f3956e4fb9ce0207d599ff143
Temur Coder <temurnarzullayev810@gmail.com>
2024-11-09 20:32:19 +0300

    first commit

---
+++ music.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "Jazzduren",
+    artist: "Jazzduren",
+    img: `jazzdauren`,
+    src: `Jazzdauren`,
+  },
+  {
+    name: "Piano",
+    artist: "Piano",
+    img: `Piano`,
+    src: `Piano`,
+  },
+  {
+    name: "Elman & Mona",
+    artist: "Elman & Mona",
+    img: `elman`,
+    src: `Elman`,
+  },
+  {
+    name: "Emin & JONY - Камин",
+    artist: "Emin & JONY",
+    img: `jony`,
+    src: `Jony`,
+  },
+];
